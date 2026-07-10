8e65270115ecb07680c9d9d02fe2d330002714bb
service-tip-git <service-tip-git@users.noreply.github.com>
2026-07-10 02:49:26 +0000

    Deploying to gh-pages from @ service-tip-git/webcomponents@9ca0ef0c4333c22a9e43c86d87c31e4a35f61e3f 🚀

--- nightly/local-cdn/fiori/dist/assets/ListItemStandardExpandableTextTemplate-aEUJhJoa.js
+++
@@ -1,1 +0,0 @@
-import{j as x,aA as m}from"./bundle.esm-14IUKDA4.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
