4aa299fc16d1f91eb30df223bea474d00e4fa36f
service-tip-git <service-tip-git@users.noreply.github.com>
2026-08-22 01:08:32 +0000

    Deploying to gh-pages from @ service-tip-git/webcomponents@29bb53aeec1476c46f42a168d188c6715d17f573 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.0",
     isNext: false,
-    buildTime: 1787274413,
+    buildTime: 1787360591,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
