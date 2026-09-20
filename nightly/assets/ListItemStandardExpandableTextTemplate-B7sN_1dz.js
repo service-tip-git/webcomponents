1847fe601c87e8af4f01929d6044cb6b7a00c590
service-tip-git <service-tip-git@users.noreply.github.com>
2026-09-20 03:22:34 +0000

    Deploying to gh-pages from @ service-tip-git/webcomponents@72e02324117c5014094a4776460f76b20fc6b853 🚀

--- nightly/assets/ListItemStandardExpandableTextTemplate-B7sN_1dz.js
+++
@@ -1,1 +0,0 @@
-import{e as x,E as m}from"./bundle.esm-BF5Za2WH.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1789786949,
+    buildTime: 1789874252,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
